1ac90dc902a140f2be98df2a9abec192c7ee4b94
PatDahl79 <saravan.dahl79@gmail.com>
2025-02-14 03:42:59 +0100

    ,
    ,

--- app/project2/page.tsx
+++ app/project2/page.tsx
@@ -1,15 +1,22 @@
 "use client";
 
-import { navItems } from "@/data";
+import { navItems, project2Items } from "@/data";
 import Footer from "@/components/Footer";
 import Clients from "@/components/Clients";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import JourneysGrid from "@/components/journeysGrid";
+
 
 const page = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
+        <section>
+          <h2 className="text-2xl font-semibold">Project 2: Traveler App</h2>
+          <JourneysGrid journeys={project2Items.Overview} />
+        </section>
+        <br />
 
         <Clients />
         <Footer />
--- app/project4/page.tsx
+++ app/project4/page.tsx
@@ -1,15 +1,21 @@
 "use client";
 
-import { navItems } from "@/data";
+import { navItems, project4Items } from "@/data";
 import Footer from "@/components/Footer";
 import Clients from "@/components/Clients";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import JourneysGrid from "@/components/journeysGrid";
+
 
 const page = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
+        <section>
+          <h2 className="text-2xl font-semibold">Project 4: Traveler App</h2>
+          <JourneysGrid journeys={project4Items.journeys} />
+        </section>
 
         <Clients />
         <Footer />
